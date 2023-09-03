5128881f2b01322442b49a6ea804c7afdad7ffb3
Dhananjay <dhananjay.latkar@gmail.com>
2023-09-03 21:49:16 +0530

    imageprocessor: Image class skeleton

---
+++ src/imageprocessor.py
@@ -0,0 +1,15 @@
+class Image:
+    """Image Object"""
+
+    def __init__(self, location) -> None:
+        self.location = location
+        self.height = None
+        self.width = None
+
+    def get_height(self) -> int:
+        """Image Height"""
+        pass
+
+    def get_width(self) -> int:
+        """Image Width"""
+        pass

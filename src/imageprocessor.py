d78de992e33a1441abc57e24baa4fc65422f49cd
Zike_01 <zikekomakoma@gmail.com>
2023-09-04 20:20:59 +0300

    Create split method and return array of image data

--- src/imageprocessor.py
+++ src/imageprocessor.py
@@ -1,3 +1,6 @@
+import base64
+import io
+
 from PIL import Image
 
 
@@ -9,6 +12,23 @@ class Im:
         self.image = Image.open(location)
         self.width, self.height = self.image.size
 
-    def split(self):
-        """Splits the image into tiles of random width, random height"""
-        pass
+    def split(self, rows, cols):
+        """Splits the image into nxn tiles"""
+        tile_width, tile_height = self.width // rows, self.height // cols
+
+        image_data = []
+        for i in range(rows):
+            for j in range(cols):
+                left = i * tile_width
+                upper = j * tile_height
+                right = left + tile_width
+                lower = upper + tile_height
+                new_img = self.image.crop((left, upper, right, lower))
+
+                # encode the new image and append to image_data
+                data = io.BytesIO()
+                new_img.save(data, "JPEG")
+                img_b64 = base64.b64encode(data.getvalue()).decode("utf-8")
+                image_data.append(img_b64)
+
+        return image_data

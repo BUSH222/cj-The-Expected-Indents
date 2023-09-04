4e4e9e3eaaa231b683daa2c03be4f9d65457e021
CodeRulerNo1 <45328456+CodeRulerNo1@users.noreply.github.com>
2023-09-04 22:50:21 +0530

    Update game.py

--- src/game.py
+++ src/game.py
@@ -48,14 +48,12 @@ class Game:
             i = letters.index(guessed_letter)
             dis[i] = letters[i]
             self.guessed_letters.append(guessed_letter)
-            # unhide part of image function
         elif guessed_letter not in letters and guessed_letter in self.guessed_letters:
             self.badletter = True
         elif guessed_letter not in letters:
             self.lives -= 1
             self.badletter = True
-            # call function to get image peices if there are none then break
-            # else look for a random image piece and turn it into pure white or pure black
+        return self.lives, "".join(dis), self.badletter
         return self.lives, "".join(dis), self.badletter
             # call function to get image peices if there are none then break
             # else look for a random image piece and turn it into pure white or pure black

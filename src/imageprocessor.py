093b25c805e98c8dc63fcd5166dd061eb0bf131c
Zike_01 <zikekomakoma@gmail.com>
2023-09-06 17:44:37 +0300

    Change methods to return PIL image object instead of encoding. place_tiles() returns merged image

--- src/imageprocessor.py
+++ src/imageprocessor.py
@@ -1,5 +1,3 @@
-import base64
-import io
 import random
 
 from PIL import Image
@@ -12,26 +10,25 @@ class Im:
         self.location = location
         self.image = Image.open(location)
         self.width, self.height = self.image.size
+
+        self.image_new = Image.new("RGB", self.image.size, (255, 255, 255))
         self.image_data = []
 
     def split_image(self):
-        """Splits the image into tiles of variable width. Returns a list of tuples -> (image data, upper-left coordinates)
-
-        Args:
-            rows (int): Number of tile rows to be included
-
-        Returns:
-            list: Encoded image data
         """
-        rows = 10
-        min_width = self.width // 25
+        Splits the image into tiles of variable width.
+
+        Returns a list of tuples -> (Pillow object, upper-left coordinates)
+        """
+        rows = 32
+        min_width = self.width // 32
         tile_height = self.height // rows
 
         for i in range(rows):
             # define the remaining space in each row
             remaining = self.width
             while remaining > 0:
-                tile_width = random.randint(min_width, remaining)
+                tile_width = random.randint(min_width, self.width // 16)
 
                 if (remaining - tile_width) < min_width:
                     tile_width = remaining
@@ -47,19 +44,9 @@ class Im:
 
                 new_img = self.image.crop((left, upper, right, lower))
 
-                # encode the new image and append to image_data
-                data = io.BytesIO()
-                new_img.save(data, "JPEG")
-                img_b64 = base64.b64encode(data.getvalue()).decode("utf-8")
-                self.image_data.append((img_b64, left, upper))
+                # add cropped image and its coordinates to image_data
+                self.image_data.append((new_img, left, upper))
         return self.image_data
-
-    def display_image(self):
-        """Returns the full image to be displayed"""
-        data = io.BytesIO()
-        self.image.save(data, "JPEG")
-
-        return base64.b64encode(data.getvalue()).decode("utf-8")
 
     def place_tiles(self, word_length):
         """Picks a random number of tiles to be placed on the game screen"""
@@ -71,7 +58,9 @@ class Im:
             tiles.append(tile)
             self.image_data.remove(tile)
 
-        return tiles
+        for tile in tiles:
+            self.image_new.paste(tile[0], (tile[1], tile[2]))
+        return self.image_new
 
     def remove_tiles(self):
         """Permantly blackens some of the placed tiles on the game board"""

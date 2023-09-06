3498ffbb3c83eee5997eee8ae7d496682cb23259
Dhananjay <dhananjay.latkar@gmail.com>
2023-09-06 22:57:58 +0530

    game: play logic

--- src/game.py
+++ src/game.py
@@ -7,66 +7,66 @@ from imageprocessor import Im
 
 
 class Game:
-    """
-    game class
-
-    2 functions and 3 data members
-    """
+    """game class"""
 
     def __init__(self, word, lives):
-        """
-        Parameters
-
-        ----------
-        word : string
-            Secret Code.
-        lives : int
-            no game no lives.
-        letter : e
-            E.
-
-        Returns
-        -------
-        None.
-
-        """
         self.word = word
         self.lives = lives
         self.guessed_letters = []
         self.wlen = len(word)
         self.dis = ["_"] * len(self.word)
-        self.res = requests.get(
-            f"https://loremflickr.com/1024/1024/{self.word}", timeout=10)
-        self.img = Image.open(BytesIO(self.res.content))
+        self.size = 1024
+        self.im = self.fetch_image()
+
+    def fetch_image(self):
+        """Fetch image from loremflickr using word
+
+        Returns:
+            Im object
+        """
+        res = requests.get(
+            f"https://loremflickr.com/{self.size}/{self.size}/{self.word}", timeout=10
+        )
+        im = Im(Image.open(BytesIO(self.res.content)))
+        im.split_image()
+        return im
 
     def play(self, guessed_letter):
-        """
-        Parameters
+        """Play the next move
 
-        ----------
-        self : word(str),lives(int),guessed_letter(string)
-        Returns
-        -------
-        lives(int),display(str),badletter(str)
+        Args:
+            guessed_letter (string): guessed letter by user
+        Returns:
+            Tuple(int, str, bool, PIL Image): (remaining_lives, display, badletter, image)
         """
-        imge = Im(self.img)
-        imge.split_image()
-        letters = self.word
-        self.badletter = False
-        if self.lives == 0:
-            return self.lives, "".join(self.dis), self.badletter, imge.image_new
-        if self.dis == letters:
-            return self.lives, "".join(self.dis), self.badletter, imge.image_new
-        if guessed_letter in letters and guessed_letter not in self.guessed_letters:
-            for j, lttr in enumerate(letters):
+        badletter = False
+
+        if guessed_letter in self.word and guessed_letter not in self.guessed_letters:
+            # New letter guessed correctly
+            for j, lttr in enumerate(self.word):
                 if guessed_letter == lttr:
-                    self.dis[j] = letters[j]
-            imge.place_tiles(letters, self.guessed_letters)
-            self.guessed_letters.append(guessed_letter)
-        if guessed_letter not in letters and guessed_letter in self.guessed_letters:
-            self.badletter = True
-        if guessed_letter not in letters:
-            self.lives -= 1
-            self.badletter = True
-            imge.remove_tiles()
-        return self.lives, "".join(self.dis), self.badletter, imge.image_new
+                    self.dis[j] = self.word[j]
+            self.im.place_tiles(letters, guessed_letter)
+
+        elif guessed_letter not in self.word:
+            # Wrong letter guessed
+            badletter = True
+
+            if guessed_letter not in self.guessed_letters:
+                # Deduct lives only for first time wrong guess
+                self.lives -= 1
+                self.im.remove_tiles()
+
+        # Remember guessed_letter
+        self.guessed_letters.add(guessed_letter)
+
+        if "".join(self.dis) == self.word:
+            # Guessed correct word. WIN!!!
+            return self.lives, "".join(self.dis), badletter, self.im.image
+
+        if self.lives == 0:
+            # No more lives. Game Over!
+            # TODO: return "game over" image
+            return self.lives, "".join(self.dis), badletter, self.im.image_new
+
+        return self.lives, "".join(self.dis), badletter, self.im.image_new

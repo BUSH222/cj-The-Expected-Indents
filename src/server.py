0b716bf9dd5eba366a15af2b70d0da4580ca72f6
Ted V <58144503+BUSH222@users.noreply.github.com>
2023-09-06 22:01:55 +0300

    Docstring update

--- src/server.py
+++ src/server.py
@@ -16,7 +16,18 @@ game_images = dict()
 
 
 def make_uid(length=6):
-    """Generate a random uid of a set length."""
+    """Generate a random uid of a set length.
+
+    Args:
+        length (int, default=6): The length of the random id. Should be 1 or more.
+
+    Raises:
+        AssertionError: Length can't be 0 or lower.
+
+    Returns:
+        str: A random uid.
+    """
+    assert length > 0
     characters = string.ascii_letters + string.digits
     chars = [secrets.choice(characters) for _ in range(length)]
     random_string = ''.join(chars)
@@ -24,7 +35,11 @@ def make_uid(length=6):
 
 
 def random_word():
-    """Generate a random word to be used in the game."""
+    """Generate a random word to be used in the game.
+
+    Returns:
+        str: A random word from the word list.
+    """
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, 'nouns-clear.txt')
     with open(file_path) as nounfile:
@@ -34,14 +49,22 @@ def random_word():
 
 @app.route('/')
 def homepage():
-    """Return the homepage for the game."""
+    """Return the homepage for the game.
+
+    Returns:
+        str: A template for the homepage.
+    """
     return render_template('home.html')
 
 
 @app.route('/start_game')
 def game_start():
-    """Start the game."""
-    global games
+    """Start the game.
+
+    Returns:
+        str: A template for the game page.
+    """
+    global games, game_images
     rword = random_word()
     game_id = make_uid()
     games[game_id] = Game(rword, 6)
@@ -51,7 +74,18 @@ def game_start():
 
 @app.route('/game/<game_id>')
 def make_move(game_id):
-    """Process and make a move."""
+    """Process and make a move.
+
+    Args:
+        game_id (str): The id for the game in which the move was made.
+
+    Raises:
+        AssertionError: Game must exist, game_id must be a key the games dictionary.
+
+    Returns:
+        dict: Updated data after the move: word status, lives, change in lives,
+                if the user guessed a letter in a word (feedback), image link.
+    """
     global games, game_images
     assert game_id in games.keys()
     cgame = games[game_id]
@@ -67,14 +101,24 @@ def make_move(game_id):
 
 @app.route('/image/<game_id>')
 def get_image(game_id):
-    """Return an image for the current game."""
+    """Return an image for the current game.
+
+    Args:
+        game_id (str): The id for the game in which the move was made.
+
+    Raises:
+        AssertionError: Game must exist, game_id must be a key the games_images dictionary.
+
+    Returns:
+        JPEG file: image for the game with an id game_id.
+    """
     global game_images
     assert game_id in game_images.keys()
     img_io = BytesIO()
     pil_img = game_images[game_id]
     pil_img.save(img_io, 'JPEG', quality=70)
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
+    return send_file(img_io, mimetype='t')
 
 
 app.run(debug=True, host='127.0.0.1', port=5000)
